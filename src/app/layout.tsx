5a20d02f87f150774291d8701a13f72a884e05e7
PedroPini <augustopini@gmail.com>
2024-08-24 22:44:11 +0800

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +19,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          themes={['light', 'dark', 'yellow']}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,177 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Menu } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import ThemeToggle from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+// import ShadcnKit from "@/components/icons/shadcn-kit";
+import { nanoid } from "nanoid";
+import Link from "next/link";
+import config from "@/config";
+import Image from "next/image";
+import logo from "@/app/icon.png";
+const Navbar = () => {
+    return (
+        <Card className="container bg-card py-3 px-4 border-0 flex items-center justify-between gap-6 rounded-2xl mt-5">
+            <div className="text-primary cursor-pointer" />
+            {/* Your logo/name on large screens */}
+            <div className="flex lg:flex-1">
+                <Link
+                    className="flex items-center gap-2 shrink-0 "
+                    href="/"
+                    title={`${config.appName} hompage`}
+                >
+                    <Image
+                        src={logo}
+                        alt={`${config.appName} logo`}
+                        className="w-8"
+                        placeholder="blur"
+                        priority={true}
+                        width={32}
+                        height={32}
+                    />
+                    <span className="font-extrabold text-lg">{config.appName}</span>
+                </Link>
+            </div>
+            <ul className="hidden md:flex items-center gap-10 text-card-foreground">
+                <li className="text-primary font-medium">
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#features">Features</a>
+                </li>
+                <li>
+                    <a href="#pricing">Pricing</a>
+                </li>
+                <li>
+                    <a href="#faqs">FAQs</a>
+                </li>
+                <li>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <span className="cursor-pointer">Pages</span>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent align="start">
+                            {landings.map((page) => (
+                                <DropdownMenuItem key={page.id}>
+                                    <Link href={page.route}>{page.title}</Link>
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </li>
+            </ul>
+
+            <div className="flex items-center">
+                <Button variant="secondary" className="hidden md:block px-2">
+                    Login
+                </Button>
+                <Button className="hidden md:block ml-2 mr-2">Get Started</Button>
+
+                <div className="flex md:hidden mr-2 items-center gap-2">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <span className="py-2 px-2 bg-gray-100 rounded-md">Pages</span>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent align="start">
+                            {landings.map((page) => (
+                                <DropdownMenuItem key={page.id}>
+                                    <Link href={page.route}>{page.title}</Link>
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" size="icon">
+                                <Menu className="h-5 w-5 rotate-0 scale-100" />
+                            </Button>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuItem>
+                                <a href="#home">Home</a>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <a href="#features">Features</a>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <a href="#pricing">Pricing</a>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <a href="#faqs">FAQs</a>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Button variant="secondary" className="w-full text-sm">
+                                    Login
+                                </Button>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Button className="w-full text-sm">Get Started</Button>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+
+                <ThemeToggle />
+            </div>
+        </Card>
+    );
+};
+
+const landings = [
+    {
+        id: nanoid(),
+        title: "Landing 01",
+        route: "/project-management",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 02",
+        route: "/crm-landing",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 03",
+        route: "/ai-content-landing",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 04",
+        route: "/new-intro-landing",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 05",
+        route: "/about-us-landing",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 06",
+        route: "/contact-us-landing",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 07",
+        route: "/faqs-landing",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 08",
+        route: "/pricing-landing",
+    },
+    {
+        id: nanoid(),
+        title: "Landing 09",
+        route: "/career-landing",
+    },
+];
+
+export default Navbar;
