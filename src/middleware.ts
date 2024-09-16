59d5e3be54b095f4278083ae693e34a907ac64e0
PedroPini <augustopini@gmail.com>
2024-09-16 18:59:25 +0800

    middleware auth protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,38 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+// import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+// export default clerkMiddleware()
+
+// export const config = {
+//     matcher: [
+//         // Skip Next.js internals and all static files, unless found in search params
+//         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//         // Always run for API routes
+//         '/(api|trpc)(.*)',
+//     ],
+// }
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+
+
+
+const isProtectedRoute = createRouteMatcher(['/csr(.*)', '/ssr(.*)'])
+const isProtectedApiRoute = createRouteMatcher([
+    "/api/webhook(.*)",
+    "/api/stripe(.*)"
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+  if (isProtectedApiRoute(req)) auth().protect()
+})
+
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
