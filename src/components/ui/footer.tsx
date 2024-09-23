fb6a831d2ddce24cf68db81a885517adea2b143d
PedroPini <augustopini@gmail.com>
2024-09-23 17:28:28 +0800

    Fixing icon on footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <a href="https://flowbite.com/" className="flex items-center">
-                            {/* <Image src={logo} className="h-8 me-3" alt="FlowBite Logo" /> */}
+                            <Image src={logo} className="h-8 w-auto me-3" alt="FlowBite Logo" />
                             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{config.appName}</span>
                         </a>
                     </div>
