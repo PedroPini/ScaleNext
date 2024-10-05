29d9e438198026d988b8977ba2cf3a690fd0407d
PedroPini <augustopini@gmail.com>
2024-10-05 14:16:35 +0800

    Adding the latest gemini integration

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -1,23 +1,28 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import { GoogleGenerativeAIStream, StreamingTextResponse } from 'ai';
-import { NextRequest } from 'next/server';
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
+import { NextRequest, NextResponse } from 'next/server';
+import { generateStreamResponse } from '@/app/api/gemini/generate-stream-response'
+import { generateTextResponse } from '@/app/api/gemini/generate-text-response'
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GEMINI_API_KEY
+});
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
-
+const googleAI = google('gemini-1.5-pro-latest');
 export async function POST(req: NextRequest) {
   try {
     const { messages } = await req.json();
     const lastMessage = messages[messages.length - 1];
+    const prompt = lastMessage.content;
 
-    const geminiStream = await genAI
-      .getGenerativeModel({ model: 'gemini-pro' })
-      .generateContentStream(lastMessage.content);
+    //generateTextResponse you just need change the ui to do a fetch.
+    //  const response = await generateTextResponse(googleAI, prompt)
+    //  return NextResponse.json(response, { status: 200 })
 
-    const stream = GoogleGenerativeAIStream(geminiStream);
+   //  Use streamText to generate and stream the response
+   return generateStreamResponse(googleAI, messages)
+  
 
-    return new StreamingTextResponse(stream);
   } catch (error) {
-    console.error(error);
-    return new Response('An error occurred while generating content', { status: 500 });
+    console.error('Error generating content:', error);
+    return new NextResponse('An error occurred while generating content', { status: 500 });
   }
 }
